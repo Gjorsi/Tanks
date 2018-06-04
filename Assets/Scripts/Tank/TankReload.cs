730e9e7573a72107942aabb9faae286c65a34a04
Carl August Gjørsvik <Carl.Gjorsvik@student.uib.no>
2018-06-04 12:58:04 +0200

    Playable 2-player local

---
+++ Assets/Scripts/Tank/TankReload.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankReload : MonoBehaviour
+{         
+    public Slider m_Slider;                        
+    public Image m_FillImage;
+    public Color m_FullLoadColor = Color.black;  
+    public Color m_ZeroLoadColor = Color.yellow;
+
+    private float m_CurrentState;
+
+    private void OnEnable()
+    {
+        SetLoadUI();
+    }
+
+
+    public void Reload(float timeStamp, float reloadTime)
+    {
+        while(Time.time < timeStamp + reloadTime)
+        {
+            m_CurrentState = (timeStamp + reloadTime - Time.time) / 2;
+            SetLoadUI();
+        }
+            
+    }
+
+
+    private void SetLoadUI()
+    {
+        // Adjust the value and colour of the slider.
+        m_Slider.value = m_CurrentState;
+
+        m_FillImage.color = Color.Lerp(m_ZeroLoadColor, m_FullLoadColor, m_CurrentState);
+    }
+}
--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -6,19 +6,23 @@ public class TankShooting : MonoBehaviour
     public int m_PlayerNumber = 1;       
     public Rigidbody m_Shell;            
     public Transform m_FireTransform;    
-    public Slider m_AimSlider;           
+    public Slider m_AimSlider;
+    public Slider m_ReloadSlider;
     public AudioSource m_ShootingAudio;  
     public AudioClip m_ChargingClip;     
     public AudioClip m_FireClip;         
     public float m_MinLaunchForce = 15f; 
     public float m_MaxLaunchForce = 30f; 
-    public float m_MaxChargeTime = 0.75f;
+    public float m_MaxChargeTime = 1f;
+    public float m_ReloadTime = 2f;
 
     
     private string m_FireButton;         
     private float m_CurrentLaunchForce;  
     private float m_ChargeSpeed;         
-    private bool m_Fired;                
+    private bool m_Fired;
+    private bool m_Charging;
+    private float m_TimeStamp = 0f;
 
 
     private void OnEnable()
@@ -51,19 +55,35 @@ public class TankShooting : MonoBehaviour
         {
             // just started holding fire button
             m_Fired = false;
+
+            if (Time.time < m_TimeStamp + m_ReloadTime)
+            {
+                m_Charging = true;
+                return;
+            }
+            
             m_CurrentLaunchForce = m_MinLaunchForce;
 
             m_ShootingAudio.clip = m_ChargingClip;
             m_ShootingAudio.Play();
         }
-        else if(Input.GetButton(m_FireButton) && !m_Fired)
+        else if(Input.GetButton(m_FireButton) && !m_Fired && (Time.time > m_TimeStamp + m_ReloadTime))
         {
             // holding fire button, not yet fired
+
+            if (m_Charging)
+            {
+                m_CurrentLaunchForce = m_MinLaunchForce;
+                m_ShootingAudio.clip = m_ChargingClip;
+                m_ShootingAudio.Play();
+                m_Charging = false;
+            }
+
             m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
 
             m_AimSlider.value = m_CurrentLaunchForce;
         }
-        else if(Input.GetButtonUp(m_FireButton) && !m_Fired)
+        else if(Input.GetButtonUp(m_FireButton) && !m_Fired && (Time.time > m_TimeStamp + m_ReloadTime))
         {
             // button released, FIRE!
             Fire();
@@ -75,7 +95,10 @@ public class TankShooting : MonoBehaviour
     private void Fire()
     {
         // Instantiate and launch the shell.
+        m_TimeStamp = Time.time;
         m_Fired = true;
+        //TankReload tankReload = GetComponentInChildren<TankReload>();
+        //tankReload.Reload(m_TimeStamp, m_ReloadTime);
 
         Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
 
